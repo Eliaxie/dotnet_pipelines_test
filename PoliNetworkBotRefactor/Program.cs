b27fc8c1d7c7b858d04e9d9778462f965d9420cf
Eliaxie <elia.maggioni.gm@gmail.com>
2023-08-01 23:36:38 +0200

    init

---
+++ PoliNetworkBotRefactor/Program.cs
@@ -0,0 +1,20 @@
+﻿// Assuming this class is part of a project referenced by your test project
+namespace PoliNetworkBotRefactor;
+
+public class HelloWorld
+{
+    public string SayHello()
+    {
+        return "Hello, World!";
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        HelloWorld helloWorld = new HelloWorld();
+        string result = helloWorld.SayHello();
+        Console.WriteLine(result);
+    }
+}
---
+++ PoliNetworkBotRefactorTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using PoliNetworkBotRefactor;
+using Xunit;
+
+namespace PoliNetworkBotRefactorTests;
+
+public class HelloWorldTests
+{
+    [Fact]
+    public void TestSayHello()
+    {
+        // Arrange
+        HelloWorld helloWorld = new HelloWorld();
+
+        // Act
+        string result = helloWorld.SayHello();
+
+        // Assert
+        Assert.Equal("Hello, World!", result);
+    }
+}
